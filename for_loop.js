db0940b2749177d7d2c012f94ae8d8897f09f6c8
Nina Stoyanova <nina.stoqnova91@gmail.com>
2021-02-06 10:11:30 +0200

    loop

--- for_loop.js
+++ for_loop.js
@@ -91,7 +91,82 @@ for (var i=0; i<10; i=i+2) { //0123456789 //02468
    console.log(i);  //print 0-1000
 }
 
+//EXCERSISEC OF JS FUNDAMENTALS $ ESSENTIALS
 
 
 
 
+var sumLength = 0; //sum of the length of all strings 
+
+var count = sumLength.length;
+
+function strings(str){ // "nina"    //// COUNT THE LETTERS OF NINA
+    
+    for (i=0; i<str.length; i++){
+        var element = str[i];  //n
+        sumLength = sumLength +1; 
+    }
+
+}
+strings("nina");
+console.log(count);
+
+
+
+
+
+
+function strings (str, str1, str2) {  //COUNT THE SUM OF THE CHARACTERS IN THE STRINGS + SUM THE AVERAGE NUMBER
+
+    
+  var sumStr = str + str1 + str2;
+  var sumLetters = sumStr.length;
+  console.log(sumLetters);
+
+  var averageLength = 0;
+  averageLength = Math.floor(sumLetters/3);
+  console.log(averageLength);
+}
+//console.log(strings("nina", "pepi", "dora"));
+
+
+
+
+
+
+// '+', '-', '*', '/', '%', '**'.  
+
+function solve(num1, num2, str){  //CALCULATE 2NUMBERS WITH STRING(SPECIAL CHARACTER)
+  var result;
+  switch(str) {
+      case "+": result = num1 + num2; break;
+      case "-": result = num1 - num2; break;
+      case "*": result = num1 * num2; break;
+      case "/": result = num1 / num2; break;
+      case "%": result = num1 % num2; break;
+      case "**": result = num1 ** num2; break;
+  }
+  console.log(result);
+
+}
+
+solve(5, 6, "*");
+
+
+
+
+
+
+function solve(n, m){  // SUM OF NUMBERS N & M
+
+  var result= 0;
+  var num1 = Number(n);
+  var num2 = Number(m);
+
+  for (var i = num1; i <= num2; i++) {
+      result = result +i;
+  }
+  return result;
+
+}
+console.log(solve("-8", "20"));
